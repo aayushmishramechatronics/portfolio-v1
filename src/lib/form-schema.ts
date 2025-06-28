7e83034cceb2d61f65078bde744dc8a93ccedccd
Aayush Mishra <manofganges@gmail.com>
2025-06-28 11:33:11 +0530

    Update form-schema.ts

--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -3,9 +3,9 @@ import { z } from 'zod';
 export const formSchema = z.object({
   email: z
     .string()
-    .min(1, { message: 'Email is required' })
-    .email({ message: 'Must be a valid email' }),
-  message: z.string().min(1, { message: 'Message is required' }),
+    .min(1, { message: 'email is required' })
+    .email({ message: 'must be a valid email' }),
+  message: z.string().min(1, { message: 'message is required' }),
 });
 
 export type TFormSchema = z.infer<typeof formSchema>;
