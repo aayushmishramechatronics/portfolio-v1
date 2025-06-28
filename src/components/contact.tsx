db4f89ae6c16b7921004a5e5a4969cc18420f677
Aayush Mishra <manofganges@gmail.com>
2025-06-28 15:58:43 +0530

    Update contact.tsx

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -113,7 +113,7 @@ export const Contact = () => {
           </label>
           <textarea
             id="message"
-            placeholder="Yo! What's Up?"
+            placeholder="Hello Aayush"
             {...register('message')}
             className={cn(
               'border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring mt-2 flex h-60 w-full resize-none rounded-md border px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
