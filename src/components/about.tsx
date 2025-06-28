fc6409419cf809f6bf54d989b2446e61fdf491ad
Aayush Mishra <manofganges@gmail.com>
2025-06-28 13:10:31 +0530

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -21,7 +21,7 @@ export const About = () => {
       <SectionHeading heading="About Me" />
       <div className="-mt-5 max-w-2xl text-center leading-7">
         <p className="mb-4">
-          I&apos;m Michał Skolak, I&apos;m a front-end developer from Poland.
+          I&apos;m Aayush Mishra, I&apos;m a full-stack developer from India.
           I&apos;ve been learning programming for over 3 years. After I started
           high school, I decided to pursue my passion for programming. My
           favorite part of programming is the problem-solving aspect. I love the
@@ -31,11 +31,6 @@ export const About = () => {
           ventures. My core stack is React, Next.js, TypeScript, and Tailwind
           CSS. I am also familiar with Nest.js and Prisma. I&apos;m always
           looking to learn new technologies.
-        </p>
-        <p>
-          I&apos;m open to Job opportunities where I can contribute, learn and
-          grow. If you have a good opportunity that matches my skills and
-          experience then don&apos;t hesitate to contact me.
         </p>
       </div>
       <Skills />
