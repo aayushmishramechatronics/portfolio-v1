e60f69cda97c61085dd7ca6341531097171d11e2
Aayush Mishra <manofganges@gmail.com>
2025-06-29 14:31:49 +0530

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -19,20 +19,25 @@ export const About = () => {
       transition={{ delay: 0.175 }}
     >
       <SectionHeading heading="About Me" />
-      <div className="-mt-5 max-w-2xl px-4 text-center leading-7 sm:px-6">
+      <div className="-mt-5 max-w-2xl text-center leading-7">
         <p className="mb-4">
           I&apos;m Aayush Mishra, a full-stack developer from India. I enjoy
           prototyping fast, debugging smarter, and scaling solutions that solve
-          real-world problems.
+          real-world problems. I explore systems that blend hardware with
+          intelligence, and I&apos;m always experimenting — whether it&apos;s
+          building CAD models, designing PCBs, creating AI-integrated web apps,
+          or automating workflows using PLCs and microcontrollers. I thrive on
+          clean code, well-structured systems, and learning something new with
+          every project I take on.
         </p>
         <p>
-          I thrive on clean code, well-structured systems, and learning
-          something new with every project I take on. Let&apos;s build something
-          awesome together.
+          I&apos;m open to collaboration, freelancing, or full-time
+          opportunities where I can contribute, learn, and grow. If you have a
+          project or role that aligns with my skills, feel free to reach out —
+          let&apos;s build something awesome together!
         </p>
       </div>
       <Skills />
     </motion.section>
   );
 };
-
